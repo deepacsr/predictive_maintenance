e78ef6f03e824bcb897716fa05efd86721d3a626
deepacsr <deepabanda@yahoo.com>
2026-05-25 16:04:24 +0000

    Updated package project from colab

--- deployment/app.py
+++ deployment/app.py
@@ -24,9 +24,9 @@ This application predicts if Engine is likely to go Faulty or working in Nornaml
 EngineRPM = st.number_input("Engine RPM", min_value=60, max_value=2240, value=791, step =50)
 LubricanOilPressure = st.number_input("Lubricant oil pressure", min_value=0.003, max_value=7.2, value=3.3, step =0.1)
 FuelPressure = st.number_input("Fuel Pressure", min_value=0.003, max_value=21.1, value=6.6, step =0.1)
-CoolantPressure = st.number_input("Coolant Pressure", min_value=0.0024, max_value=7472, value=2.33, step =0.1)
-LubricantOilTemp = st.number_input("Lubricant Oil Temp", min_value=71.3, max_value=89.58, value=77.64, step =1)
-CoolantTemp = st.number_input("Coolant Temp", min_value=61.67, max_value=195.52, value=78.42, step =1)
+CoolantPressure = st.number_input("Coolant Pressure", min_value=0.0024, max_value=7472.0, value=2.33, step =0.1)
+LubricantOilTemp = st.number_input("Lubricant Oil Temp", min_value=71.3, max_value=89.58, value=77.64, step =1.0)
+CoolantTemp = st.number_input("Coolant Temp", min_value=61.67, max_value=195.52, value=78.42, step =1.0)
 
 # Assemble input into DataFrame
 input_data = pd.DataFrame([{
